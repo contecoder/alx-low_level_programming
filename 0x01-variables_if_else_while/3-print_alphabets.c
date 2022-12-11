a93633e3e9df78870886be38707565a9c9a24277
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-11 03:37:52 -0800

    upper lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ *main- betty style doc for function main goes there
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+int i;
+char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+for (i = 0 ; i <= 52 ; i++)
+{
+putchar(alphabet[i]);
+}
+putchar('\n');
+return (0);
+}
