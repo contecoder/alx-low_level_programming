699dfe1a62e55b1728b1533088167c05b717dac7
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-12 01:51:33 -0800

    alphabet reverse

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+/* more headers goes there */
+/**
+ *main- betty style doc for function main goes there
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+int i;
+ 
+for (i = 0 ; i <= 9 ; i++)
+{
+putchar(strcat("",i));
+}
+putchar('\n');
+return (0);
+}
