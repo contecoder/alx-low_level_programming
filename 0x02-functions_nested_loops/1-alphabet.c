4fceec28fe19a7e6f66ea4043c3dde6ac85ab70a
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-13 08:11:31 -0800

    function printing all alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/* more headers goes there */
+/**
+ *main- the entry point
+ *
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+void print_alphabet(void)
+{
+int i;
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+for (i = 0 ; i <= 25 ; i++)
+{
+putchar(alphabet[i]);
+}
+putchar('\n');
+}
