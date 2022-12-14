7a6213bddd20da824ab8115106f36ac0563fedec
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-14 09:24:50 -0800

    ok

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ else if (n > 98)
 {
 for (i = n ; i >= 98 ; i--)
 {
-if(i == 98)
+if (i == 98)
 {
 printf("98");
 }
@@ -39,7 +39,7 @@ printf("98");
 }
 else
 {
-  printf("%d, ", i);
+printf("%d, ", i);
 }
 }
 }
