db0936ef72a8e649e051f1f1da5ca41115d9d6f2
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-14 10:32:30 -0800

    all is ok

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,10 +13,20 @@ for (i = 0 ; i <= 9 ; i++)
 {
 for (j = 0 ; j <= 9 ; j++)
 {
+if ((i * j) >= 10)
+{
 if (j == 9)
 printf("%d", i * j);
 else
 printf("%d, ", i * j);
+}
+else
+{
+if (j == 9)
+printf("%d", i * j);
+else
+printf("%d,  ", i * j);
+}
 }
 printf("\n");
 }
