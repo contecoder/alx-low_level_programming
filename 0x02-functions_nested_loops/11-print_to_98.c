48885df4b45b63304ec2964c455f5a2870978d2c
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-14 04:04:07 -0800

    98 to others

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *print_to_98- that function print alphabet 10 times
+ *@n: first arg
+ *
+ *
+ *
+ *
+ */
+void print_to_98(int n)
+{
+int i;
+if (n == 98)
+{
+printf("98");
+}
+else if (n > 98)
+{
+for (i = n ; i >= 98 ; i--)
+{
+printf("%d ", i);
+}
+}
+else
+{
+for (i = n ; i >= 98 ; i++)
+{
+printf("%d ", i);
+}
+}
+printf("\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
 #include<string.h>
@@ -6,3 +5,4 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int);
 int add(int, int);
+void print_to_98(int);
