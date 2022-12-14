a84fd645b5362aff090c683e37324491fc13e9ad
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-14 10:10:00 -0800

    times table ok

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *times_table- that function print alphabet 10 times
+ *
+ *
+ */
+void times_table(void)
+{
+int i;
+int j;
+for (i = 0 ; i <= 9 ; i++)
+{
+for (j = 0 ; j <= 9 ; j++)
+{
+if (j == 9)
+printf("%d", i * j);
+else
+printf("%d, ", i * j);
+}
+printf("\n");
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ void print_alphabet_x10(void);
 int _islower(int);
 int add(int, int);
 void print_to_98(int);
+void times_table(void);
