d39fcb5c53b770ffdc8d09b8e092cbe740e3900e
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-15 12:36:46 -0800

    teste ok

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,15 +3,22 @@
  * main - causes an infinite loo
  * Return: 0
  */
+
 int main(void) 
 {
+  
 int i;
+ 
 printf("Infinite loop incoming :(\n");
+ 
 i = 0;
-/* while (i < 10)    
-{     
-putchar(i); 
+ 
+ /*while (i < 10)*/
+/*{
+  tchar(i); 
 } */
+ 
 printf("Infinite loop avoided! \\o/\n");
+
 return (0); 
 }
