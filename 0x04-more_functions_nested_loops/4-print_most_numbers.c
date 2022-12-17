34da66f511c445ca1eec5d9b08f859c10a529244
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-16 17:38:10 -0800

    not 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ *print_most_numbers- this a descripition
+ *
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+if(i != 2 || i != 4)
+putchar(i + '0');
+}
+putchar('\n');
+}
