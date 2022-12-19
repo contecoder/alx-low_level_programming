926b62c05c945369002e675c4177138c32b2544d
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-19 12:28:08 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ *print_rev- the function that change value
+ *@str: the pointer of n
+ *
+ */
+void print_rev(char *str)
+{
+int i;
+i = strlen(str);
+while (i >= 0)
+{
+putchar(*(str + i));
+i--;
+}
+putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
 int _strlen(char *str);
+void print_rev(char *str);
