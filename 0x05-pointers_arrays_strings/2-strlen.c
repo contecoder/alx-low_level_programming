48b456f5497c7e85536574adb471d85b76853007
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-19 11:38:57 -0800

    size of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ *_strlen- the function that change value
+ *@str: the pointer of n
+ *Return: the size of the value
+ */
+int _strlen(char *str)
+{
+int stringSize = strlen(str);
+return (stringSize);
+}
