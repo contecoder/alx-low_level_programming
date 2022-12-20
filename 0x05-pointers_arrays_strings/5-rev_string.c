3294d3ddc0af68c5cfb15d5ec46182985fd3e201
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-20 02:48:42 -0800

    reverse world

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,11 +3,26 @@
 #include<string.h>
 /**
  *rev_string- the function that change value
- *@str: the pointer of n
+ *@s: the pointer of n
  *
  */
-void rev_string(char *str)
+void rev_string(char *s)
 {
-  
-  printf("%s",strrev(str));
+int i;
+int j;
+char reverse[10];
+i = strlen(s) - 1;
+j = 0;
+while (i >= 0)
+{
+reverse[j] = *(s + i);
+i--;
+j++;
+}
+i = 0;
+while (i <= 10)
+{
+*(s + i) = reverse[i];
+i++;
+}
 }
