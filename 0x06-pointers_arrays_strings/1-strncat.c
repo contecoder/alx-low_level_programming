a32def80f63f92cfe3758a4193abab13542f2a14
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-21 07:13:09 -0800

    using strncpy

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ *_strncat- check the
+ *@dest: the first
+ *@src: the seconde
+ *@n: taille
+ *Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+return (strncat(dest, src, n));
+}
