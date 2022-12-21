80df89f94282f06eabe93462d243630b53d95a1b
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-21 06:16:30 -0800

    concat teste

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ *_strcat- check the
+ *@dest: the first
+ *@src: the seconde
+ *Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
