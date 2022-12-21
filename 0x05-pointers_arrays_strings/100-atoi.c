89750828c3dc4e534c5814b45eddce039ccbadef
contecoder <aboubacarsidikiconteprofs@gmail.com>
2022-12-21 14:51:01 -0800

    to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+/**
+ *_atoi- the function that change value
+ *@n: the pointer of n
+ *@a: a value
+ */
+int _atoi(char *s)
+{
+return (atoi(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ void print_array(int *a, int n);
 void puts2(char *str);
 void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
