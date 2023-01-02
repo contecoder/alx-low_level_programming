d923190297d69e674c6aab72103edd32f117a12d
contecoder <aboubacarsidikiconteprofs@gmail.com>
2023-01-02 08:08:04 -0800

    using strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ *_strspn- check the
+ *@s: the first
+ *@accept: seconde
+ *Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+return (strspn(s, accept));
+}
