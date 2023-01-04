d67c5e064718a7d084633a90534df20216efbc2b
contecoder <aboubacarsidikiconteprofs@gmail.com>
2023-01-04 13:25:16 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ *factorial - check the code
+ *@n: the first argument
+ *Return: 0
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+return (n * factorial(n - 1));
+}
---
+++ 0x08-recursion/3-factorial.c~
@@ -0,0 +1,12 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ *_strlen_recursion - check the code
+ *@s: the first argument
+ *Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+return (strlen(s));
+}
