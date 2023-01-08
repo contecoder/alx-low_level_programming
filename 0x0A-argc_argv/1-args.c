602d87aaca3305b44162c7d1f353acb44da43903
contecoder <aboubacarsidikiconteprofs@gmail.com>
2023-01-08 09:38:36 -0800

    showing number of arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+/**
+ * main - check the code
+ *@argc: the fists
+ *@argv: the second
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+argv[0] = argv[0];
+printf("%d\n", argc);
+return (0);
+}
