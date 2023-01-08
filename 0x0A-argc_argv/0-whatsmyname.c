984301b0fa22ded8f34f8c51afac0b941a3c8c0f
contecoder <aboubacarsidikiconteprofs@gmail.com>
2023-01-08 09:28:41 -0800

    print the program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+/**
+ * main - check the code
+ *@argc: the fists
+ *@argv: the second
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+argc = argc;
+puts(argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/main.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * main - check the code
+ *@argc: the fists
+ *@argv: the second
+ * Return: Always 0.
+ */
+int main(argc, char *argv)
+{
+puts(argv[0]);
+return (0);
+}
